b255a266570ea19870dfed9c7d9bbb4b22e2e2df
Ciarán <ciaranodc@gmail.com>
2020-04-28 00:32:06 +0100

    Add code for Lab3a_IntentsLab.

--- Lab3a_IntentsLab/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ Lab3a_IntentsLab/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -14,11 +14,11 @@ public class ExplicitlyLoadedActivity extends Activity {
 	static private final String TAG = "Lab-Intents";
 
 	private EditText mEditText;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.explicitly_loaded_activity);
 
 		// Get a reference to the EditText field
@@ -34,25 +34,29 @@ public class ExplicitlyLoadedActivity extends Activity {
 			public void onClick(View v) {
 
 				enterClicked();
-			
+
 			}
 		});
 
 	}
 
 	// Sets result to send back to calling Activity and finishes
-	
+
 	private void enterClicked() {
 
 		Log.i(TAG,"Entered enterClicked()");
-		
+
 		// TODO - Save user provided input from the EditText field
+		String userInput = mEditText.getText().toString();
 
 		// TODO - Create a new intent and save the input from the EditText field as an extra
-		
-		// TODO - Set Activity's result with result code RESULT_OK
-		
-		// TODO - Finish the Activity
+		Intent textIntent = new Intent(this, ActivityLoaderActivity.class);
+		textIntent.putExtra("USER_INPUT", userInput);
 
+		// TODO - Set Activity's result with result code RESULT_OK
+		setResult(RESULT_OK, textIntent);
+
+		// TODO - Finish the Activity
+		finish();
 	}
 }

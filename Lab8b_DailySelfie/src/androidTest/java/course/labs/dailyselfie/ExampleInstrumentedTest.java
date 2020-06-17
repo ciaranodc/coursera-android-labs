2a01419e12662e7cf0ba381b5a97c9ffc7d04ebf
Ciarán <ciaranodc@gmail.com>
2020-06-17 01:32:49 +0100

    Lab8b-DailySelfie: Add toolbar with camera button.

--- Lab8b_DailySelfie/src/androidTest/java/course/labs/dailyselfie/ExampleInstrumentedTest.java
+++ Lab8b_DailySelfie/src/androidTest/java/course/labs/dailyselfie/ExampleInstrumentedTest.java
@@ -1,13 +1,25 @@
 package course.labs.dailyselfie;
 
 import android.content.Context;
+import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
 
 /**
@@ -17,11 +29,28 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+
+    @Before
+    public void setUp() {
+        mActivityRule.getActivity();
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("course.labs.dailyselfie", appContext.getPackageName());
+    }
+
+    @Test
+    public void toolbarShowsCorrectText() {
+        onView(withId(R.id.my_toolbar))
+                .check(matches(isDisplayed()));
+        onView(allOf(instanceOf(TextView.class), withParent(withId(R.id.my_toolbar))))
+                .check(matches(withText("Daily Selfie")));
     }
 }
--- Lab8b_DailySelfie/src/main/java/course/labs/dailyselfie/MainActivity.java
+++ Lab8b_DailySelfie/src/main/java/course/labs/dailyselfie/MainActivity.java
@@ -1,8 +1,10 @@
 package course.labs.dailyselfie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle(R.string.toolbar_text);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
